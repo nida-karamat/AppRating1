ce9cf242e899e0c9080ddddf6308b722b855655a
nida karamat <nidakaramat2812@gmail.com>
2025-03-09 01:58:38 -0800

    merge fixes

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react(),tailwindcss(),],
-=======
-  plugins: [react(), tailwindcss()],
->>>>>>> fb55ff398d989a966f2006b1a87da672ef9a4c17
 })
